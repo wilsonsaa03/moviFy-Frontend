1a5784eef30ea1662cfcc462309ef867339b3f4c
Diego Torres <diego.torres.caicedo@correounivalle.edu.co>
2026-04-06 22:58:40 -0500

    landing 90%

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Landing } from './pages/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Landing
+  }
+];
