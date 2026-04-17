815e5bd3a6762c2c864f8f09c2faa7164d254530
Diego Torres <diego.torres.caicedo@correounivalle.edu.co>
2026-04-17 00:23:42 -0500

    Fix rutas, conflicto resuelto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
+
 import { LandingComponent } from './pages/landing/landing';
 import { LoginComponent } from './Controlador/login.component';
+import { Registro } from './pages/registro/registro';
+import { RegistroUsuario } from './pages/registro-usuario/registro-usuario';
+import { RegistroConductor } from './pages/registro-conductor/registro-conductor';
+import { RegistroAdmin } from './pages/registro-admin/registro-admin';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+
   { path: 'landing', component: LandingComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'landing', pathMatch: 'full' }, // Si entran a la raíz, ven la landing
-  { path: '**', redirectTo: 'landing' } // Por seguridad, cualquier error vuelve a la landing
+
+  { path: 'registro', component: Registro },
+  { path: 'registro/usuario', component: RegistroUsuario },
+  { path: 'registro/conductor', component: RegistroConductor },
+  { path: 'registro/admin', component: RegistroAdmin },
+
+  { path: '**', redirectTo: 'landing' }
 ];
