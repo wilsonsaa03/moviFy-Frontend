d391a0eb0a34557cb056251e8710e452c4b69c51
EdgarG1013 <edgarstiven46@gmail.com>
2026-04-19 00:41:01 -0500

    cambio el en outputMode y solucion pequeño error de importacion del usuario.servive

--- src/app/Base_de_datos/usuario.service.ts
+++ src/app/Base_de_datos/usuario.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Usuario } from '../Modelo/usuario.model';
 
 @Injectable({
   providedIn: 'root'
