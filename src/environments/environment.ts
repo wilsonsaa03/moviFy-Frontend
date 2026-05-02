748d0824dd64d4f854ecf3183ddcbde9246b198a
Wilson Steven Saa <wilsonsaa02@gmail.com>
2026-05-02 01:14:12 -0500

    Actualizo backend MoviFY

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://TU-DOMINIO-O-IP:8080/api' // Cambiar por la URL real del backend en producción
+  apiUrl: 'http://localhost:8080/api'
 };
