a0912a3df1f52cf8d00e0aa064bd85a4d94e9922
Wilson Steven Saa <wilsonsaa02@gmail.com>
2026-06-03 21:26:31 -0500

    fix: actualizacion preparacion servidor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,57 +1,31 @@
 import { Routes } from '@angular/router';
-
-// IMPORTACIONES DE PÁGINAS
 import { LandingComponent } from './pages/landing/landing';
-import { LoginComponent } from './Controlador/login.component';
-
-import { Registro } from './pages/registro/registro';
-import { RegistroUsuario } from './pages/registro-usuario/registro-usuario';
-import { RegistroConductor } from './pages/registro-conductor/registro-conductor';
-import { RegistroAdmin } from './pages/registro-admin/registro-admin';
-
-import { HomeUsuarioComponent } from './pages/home-usuario/home-usuario';
-import { HomeConductorComponent } from './pages/home-conductor/home-conductor';
-import { HomeAdminComponent } from './pages/home-admin/home-admin';
-
-import { OlvidePasswordComponent } from './pages/olvide-password/olvide-password';
-import { RestablecerPasswordComponent } from './pages/restablecer-password/restablecer-password';
-
-import { PerfilUsuarioComponent } from './pages/perfil-usuario/perfil-usuario.component';
-
-// IMPORTACIÓN DE SOLICITAR TRANSPORTE
-// Nota: Asegúrate que el nombre del archivo en la carpeta sea 'solicitar-transporte.ts'
-import { SolicitarTransporte } from './pages/solicitar-transporte/solicitar-transporte';
-import { SolicitarDomicilio } from './pages/solicitar-domicilio/solicitar-domicilio';
-import { SolicitarEncomienda } from './pages/solicitar-encomienda/solicitar-encomienda';
-import { VerMiPerfilConductorComponent } from './pages/ver-mi-perfil-conductor/ver-mi-perfil-conductor';
-import { MisViajesConductorComponent } from './pages/mis-viajes-conductor/mis-viajes-conductor';
-import { GananciasConductorComponent } from './pages/ganancias-conductor/ganancias-conductor';
 
 export const routes: Routes = [
   // Inicio y Auth
   { path: '', component: LandingComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'olvide-password', component: OlvidePasswordComponent },
-  { path: 'restablecer-password', component: RestablecerPasswordComponent },
+  { path: 'login', loadComponent: () => import('./Controlador/login.component').then(m => m.LoginComponent) },
+  { path: 'olvide-password', loadComponent: () => import('./pages/olvide-password/olvide-password').then(m => m.OlvidePasswordComponent) },
+  { path: 'restablecer-password', loadComponent: () => import('./pages/restablecer-password/restablecer-password').then(m => m.RestablecerPasswordComponent) },
 
   // Registros
-  { path: 'registro', component: Registro },
-  { path: 'registro/usuario', component: RegistroUsuario },
-  { path: 'registro/conductor', component: RegistroConductor },
-  { path: 'registro/admin', component: RegistroAdmin },
+  { path: 'registro', loadComponent: () => import('./pages/registro/registro').then(m => m.Registro) },
+  { path: 'registro/usuario', loadComponent: () => import('./pages/registro-usuario/registro-usuario').then(m => m.RegistroUsuario) },
+  { path: 'registro/conductor', loadComponent: () => import('./pages/registro-conductor/registro-conductor').then(m => m.RegistroConductor) },
+  { path: 'registro/admin', loadComponent: () => import('./pages/registro-admin/registro-admin').then(m => m.RegistroAdmin) },
 
   // Dashboards (Home)
-  { path: 'home-usuario', component: HomeUsuarioComponent },
-  { path: 'conductor', component: HomeConductorComponent },
-  { path: 'admin', component: HomeAdminComponent },
+  { path: 'home-usuario', loadComponent: () => import('./pages/home-usuario/home-usuario').then(m => m.HomeUsuarioComponent) },
+  { path: 'conductor', loadComponent: () => import('./pages/home-conductor/home-conductor').then(m => m.HomeConductorComponent) },
+  { path: 'admin', loadComponent: () => import('./pages/home-admin/home-admin').then(m => m.HomeAdminComponent) },
 
   // Perfil y Acciones de Usuario
-  { path: 'perfil-conductor', component: VerMiPerfilConductorComponent },
-  { path: 'solicitar-transporte', component: SolicitarTransporte },
-  { path: 'solicitar-domicilio', component: SolicitarDomicilio },
-  { path: 'solicitar-encomienda', component: SolicitarEncomienda },
-  { path: 'mis-viajes-conductor', component: MisViajesConductorComponent },
-  { path: 'ganancias-conductor', component: GananciasConductorComponent },
+  { path: 'perfil-conductor', loadComponent: () => import('./pages/ver-mi-perfil-conductor/ver-mi-perfil-conductor').then(m => m.VerMiPerfilConductorComponent) },
+  { path: 'solicitar-transporte', loadComponent: () => import('./pages/solicitar-transporte/solicitar-transporte').then(m => m.SolicitarTransporte) },
+  { path: 'solicitar-domicilio', loadComponent: () => import('./pages/solicitar-domicilio/solicitar-domicilio').then(m => m.SolicitarDomicilio) },
+  { path: 'solicitar-encomienda', loadComponent: () => import('./pages/solicitar-encomienda/solicitar-encomienda').then(m => m.SolicitarEncomienda) },
+  { path: 'mis-viajes-conductor', loadComponent: () => import('./pages/mis-viajes-conductor/mis-viajes-conductor').then(m => m.MisViajesConductorComponent) },
+  { path: 'ganancias-conductor', loadComponent: () => import('./pages/ganancias-conductor/ganancias-conductor').then(m => m.GananciasConductorComponent) },
 
   // Manejo de rutas no encontradas (SIEMPRE AL FINAL)
   { path: '**', redirectTo: '' }
